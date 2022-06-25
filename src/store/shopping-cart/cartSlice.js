4bed6aac166c59a4c0c8fa37ae7bd6689a8f7635
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-06-25 11:42:47 +0330

    delet item

--- src/store/shopping-cart/cartSlice.js
+++ src/store/shopping-cart/cartSlice.js
@@ -6,7 +6,8 @@ import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
 const initialState = {
     cartItems: [],
     totalQuantity:0,
-    totalAmount:0
+    totalAmount:0,
+    
 }
 
 const cartSlice = createSlice({
@@ -14,9 +15,11 @@ const cartSlice = createSlice({
         initialState: initialState,
     
         reducers:{
+
+            //add item
             addItem(state,action){
-                const newItem = action.payload
-                const existingItem = state.cartItems.find(item => item.id ===
+                const newItem = action.payload;;
+                const existingItem = state.cartItems.find((item) => item.id ===
                     newItem.id)
                     state.totalQuantity++;
     
@@ -27,7 +30,7 @@ const cartSlice = createSlice({
                             image01:newItem.image01,
                             price:newItem.price,
                             quantity:1,
-                            totalPrice:newItem.totalPrice
+                            totalPrice:newItem.price
                         })
                     }
     
@@ -63,6 +66,24 @@ const cartSlice = createSlice({
                 );
               },
 
+
+              //delet item
+
+              deleteItem(state, action) {
+                const id = action.payload;
+                const existingItem = state.cartItems.find((item) => item.id === id);
+          
+                if (existingItem) {
+                  state.cartItems = state.cartItems.filter((item) => item.id !== id);
+                  state.totalQuantity = state.totalQuantity - existingItem.quantity;
+                }
+          
+                state.totalAmount = state.cartItems.reduce(
+                  (total, item) => total + Number(item.price) * Number(item.quantity),
+                  0
+                );
+              },
+           
         },
     });
 
